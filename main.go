eadddc7ae11b49d3086d50d61a65f3e22756c291
khalad007 <www.arifhossain49919@gmail.com>
2024-06-13 18:14:04 +0600

    go variable

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello \n World")
+// 	fmt.Print("This is \t only print")
+// 	fmt.Printf("\"This is print F\"")
+// }
+
 package main
 
 import "fmt"
 
+// func main() {
+// 	fmt.Println("Hello world")
+// }
+
 func main() {
-	fmt.Println("Hello \n World")
-	fmt.Print("This is \t only print")
-	fmt.Printf("\"This is print F\"")
+	var number int16 = 400
+	fmt.Println(number)
 }
