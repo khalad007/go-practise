fdb0931fd5c09144fb03105157a9c5a3363f2c6a
khalad007 <www.arifhossain49919@gmail.com>
2024-06-13 17:54:00 +0600

    Go practise

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-pract
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello \n World")
+	fmt.Print("This is \t only print")
+	fmt.Printf("\"This is print F\"")
+}
