f6f7142f9f9ee8a09a964c530dfa83c4293ca796
khalad007 <www.arifhossain49919@gmail.com>
2024-06-18 00:01:42 +0600

    Map (obj like js)

--- main.go
+++ main.go
@@ -64,14 +64,25 @@
 // 	fmt.Println(a)
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+// 	var s []int = primes[1:4]
+
+// 	fmt.Println(s)
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-	primes := [6]int{2, 3, 5, 7, 11, 13}
+	m := make(map[string]int)
+	m["ONE"] = 1
 
-	var s []int = primes[1:4]
-
-	fmt.Println(s)
+	fmt.Println(m)
 }
