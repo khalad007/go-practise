8f8f2371335d1142caadccdf70d8bb35471025f7
khalad007 <www.arifhossain49919@gmail.com>
2024-06-18 23:36:52 +0600

    Use for loop as a while loop

--- main.go
+++ main.go
@@ -87,15 +87,29 @@
 // 	fmt.Println(m)
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	sum := 0
+// 	for i := 0; i < 10; i++ {
+// 		sum += i
+// 	}
+
+// 	fmt.Println(sum)
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-
 	sum := 0
-	for i := 0; i < 10; i++ {
-		sum += i
+
+	for sum < 1000 {
+		sum += sum
 	}
 
 	fmt.Println(sum)
